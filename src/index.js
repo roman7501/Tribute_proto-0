c6351cc378bf674400801300fd1292ac4981c4ce
Roman <romanjeanelie@yahoo.fr>
2021-03-31 16:05:16 +0200

    new version progress

--- src/index.js
+++ src/index.js
@@ -5,17 +5,3 @@ const init = () => {
 };
 
 window.onLoad = init();
-
-// const title = document.querySelector(".title");
-
-// console.log(title);
-
-// title.addEventListener("mouseenter", () => {
-//   console.log("test");
-// });
-
-// const moonMap = document.querySelector(".moon-map__container");
-// console.log(moonMap);
-// moonMap.addEventListener("mousedown", () => {
-//   console.log("enter 4");
-// });
